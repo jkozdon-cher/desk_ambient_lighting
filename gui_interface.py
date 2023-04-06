decb2524dfec3d08c2bf1607dd54cf874c2ccf3e
Z0155677 <Zf!!202206>
2023-04-06 17:20:58 +0200

    working version of gui and uart communication

--- gui_interface.py
+++ gui_interface.py
@@ -1,7 +1,6 @@
 import tkinter as tk
-import time
 from tkinter import ttk
-# from uart_comm import *
+from uart_comm import *
 
 
 class LightControl(ttk.Frame):
@@ -15,110 +14,114 @@ class LightControl(ttk.Frame):
         self.blue = tk.DoubleVar()
         self.green = tk.DoubleVar()
         self.val = tk.DoubleVar()
+        self.last_message = ''
+        self.bottom_width = 12
 
-        self.bottom_widht = 12
-
-        # arduino = ArduinoSerial.set_serial()
-        
-        tk.Button(self,
-                text='RED',
-                width=self.bottom_widht,
-                bg='red',
-                command=lambda: self.set_colors(255, 0, 0)
-                ).grid(column=0, row=1)
+        self.arduino = ArduinoSerial.set_serial()
 
         tk.Button(self,
-                text='GREEN',
-                width=self.bottom_widht,
-                bg='green',
-                command=lambda: self.set_colors(0, 255, 0)
-                ).grid(column=1, row=1)
+                  text='RED',
+                  width=self.bottom_width,
+                  bg='red',
+                  command=lambda: self.set_colors(255, 0, 0)
+                  ).grid(column=0, row=1)
 
         tk.Button(self,
-                text='BLUE',
-                width=self.bottom_widht,
-                bg='blue',
-                fg='white',
-                command=lambda: self.set_colors(0, 0, 255)
-                ).grid(column=2, row=1)
+                  text='GREEN',
+                  width=self.bottom_width,
+                  bg='green',
+                  command=lambda: self.set_colors(0, 255, 0)
+                  ).grid(column=1, row=1)
 
         tk.Button(self,
-                text='COLD WHITE',
-                width=self.bottom_widht,
-                bg='white',
-                command=lambda: self.set_colors(244, 253, 255)
-                ).grid(column=3, row=0)
+                  text='BLUE',
+                  width=self.bottom_width,
+                  bg='blue',
+                  fg='white',
+                  command=lambda: self.set_colors(0, 0, 255)
+                  ).grid(column=2, row=1)
 
         tk.Button(self,
-                text='WARM WHITE',
-                width=self.bottom_widht,
-                bg='white',
-                command=lambda: self.set_colors(243, 231, 211)
-                ).grid(column=3, row=1)
+                  text='COLD WHITE',
+                  width=self.bottom_width,
+                  bg='white',
+                  command=lambda: self.set_colors(244, 253, 255)
+                  ).grid(column=3, row=0)
 
         tk.Button(self,
-                text='YELLOW',
-                width=self.bottom_widht,
-                bg='yellow',
-                command=lambda: self.set_colors(255, 255, 0)
-                ).grid(column=3, row=2, sticky=tk.N)
+                  text='WARM WHITE',
+                  width=self.bottom_width,
+                  bg='white',
+                  command=lambda: self.set_colors(243, 231, 211)
+                  ).grid(column=3, row=1)
 
         tk.Button(self,
-                text='ON',
-                width=self.bottom_widht,
-                bg='white',
-                command=lambda: self.turn_on()
-                ).grid(column=3, row=2, sticky=tk.S)
+                  text='YELLOW',
+                  width=self.bottom_width,
+                  bg='yellow',
+                  command=lambda: self.set_colors(255, 255, 0)
+                  ).grid(column=3, row=2, sticky=tk.N)
 
         tk.Button(self,
-                text='OFF',
-                width=self.bottom_widht,
-                bg='black',
-                fg='white',
-                command=lambda: self.turn_off()
-                ).grid(column=3, row=3, sticky=tk.N)
+                  text='ON',
+                  width=self.bottom_width,
+                  bg='white',
+                  command=lambda: self.turn_on()
+                  ).grid(column=3, row=2, sticky=tk.S)
 
+        tk.Button(self,
+                  text='OFF',
+                  width=self.bottom_width,
+                  bg='black',
+                  fg='white',
+                  command=lambda: self.turn_off()
+                  ).grid(column=3, row=3, sticky=tk.N)
 
-        self.red_scale = tk.Scale(self, variable=self.red, from_=255, to=0, troughcolor='red', command=self.get_color_value)
+        self.red_scale = tk.Scale(self, variable=self.red, from_=255, to=0, troughcolor='red',
+                                  command=self.get_color_value)
         self.red_scale.grid(column=0, row=2)
 
-        self.green_scale = tk.Scale(self, variable=self.green, from_=255, to=0, troughcolor='green', command=self.get_color_value)
+        self.green_scale = tk.Scale(self, variable=self.green, from_=255, to=0, troughcolor='green',
+                                    command=self.get_color_value)
         self.green_scale.grid(column=1, row=2)
 
-        self.blue_scale = tk.Scale(self, variable=self.blue, from_=255, to=0, troughcolor='blue', command=self.get_color_value)
+        self.blue_scale = tk.Scale(self, variable=self.blue, from_=255, to=0, troughcolor='blue',
+                                   command=self.get_color_value)
         self.blue_scale.grid(column=2, row=2)
 
-        # brightness_scale = tk.Scale(self, variable=self.val, from_=0, to=255, command=get_color_value, orient=tk.HORIZONTAL)
-        # brightness_scale.place(x=140, y=200)
+        self.brightness_scale = tk.Scale(self, variable=self.val, from_=5, to=255, command=self.get_color_value,
+                                         orient=tk.HORIZONTAL)
+        self.brightness_scale.grid(column=1, row=3)
 
-        # bright_label = tk.Label(self, text='BRIGHTNESS')
-        # bright_label.place(x=60, y=220)
+        bright_label = tk.Label(self, text='BRIGHTNESS')
+        bright_label.grid(column=0, row=3)
+
         self.grid(padx=20, pady=20, sticky=tk.NSEW)
 
     def set_colors(self, *values):
-        time.sleep(0.2)
         self.red_scale.set(values[0])
         self.green_scale.set(values[1])
         self.blue_scale.set(values[2])
 
-
-# def set_brightness(value):
-#     brightness_scale.set(value)
-
+    # def set_brightness(value):
+    #     brightness_scale.set(value)
 
     def get_color_value(self, value):
-        # time.sleep(0.2)
-        message = f'{int(self.red.get())},{int(self.green.get())},{int(self.blue.get())}\n'
-        print(message)
-        # arduino.write(message.encode())
+        red_color = int(self.red.get() * self.brightness_scale.get() / 255)
+        green_color = int(self.green.get() * self.brightness_scale.get() / 255)
+        blue_color = int(self.blue.get() * self.brightness_scale.get() / 255)
 
+        message = f'{red_color},{green_color},{blue_color}\n'
+        if self.last_message != message:
+            print(message)
+            self.last_message = message
+            self.arduino.write(message.encode())
 
-    def turn_on():
-        pass
+    def turn_on(self):
+        self.arduino.write(self.last_message.encode())
 
-
-    def turn_off():
-        pass
+    def turn_off(self):
+        self.arduino.write('0,0,0\n'.encode())
 
 
 class App(tk.Tk):
@@ -131,8 +134,7 @@ class App(tk.Tk):
         self.screen_height = self.winfo_screenheight()
         self.right_position = int((self.screen_width / 2) - 220)
         self.down_position = int((self.screen_height / 2) - 220)
-        self.geometry(f'440x440+{self.right_position}+{self.down_position}')
-        
+        self.geometry(f'440x300+{self.right_position}+{self.down_position}')
 
 
 if __name__ == '__main__':
--- uart_comm.py
+++ uart_comm.py
@@ -4,8 +4,7 @@ import serial.tools.list_ports
 
 class ComPort:
     @staticmethod
-    def find_com_port(board_name='Urządzenie szeregowe USB'):
-        # Arduino nano every board_name='CH340'
+    def find_com_port(board_name='NANO'):
         ports = list(serial.tools.list_ports.comports())
         for port in ports:
             if board_name in port[1]:
@@ -20,5 +19,3 @@ class ArduinoSerial:
         com_port = ComPort.find_com_port()
         if com_port:
             return serial.Serial(com_port, 9600, timeout=0.01)
-
-# arduino = ArduinoSerial.set_serial()
